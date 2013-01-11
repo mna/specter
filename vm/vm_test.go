160bfd01b98c04adfb961891a76dc59f338afcd2
Martin Angers <martin.n.angers@gmail.com>
2013-01-10 21:23:49 -0500

    add test file to run all examples at once

---
+++ vm/vm_test.go
@@ -0,0 +1,39 @@
+package vm
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func TestExamples(t *testing.T) {
+	const dir string = "../cmd/examples"
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, fi := range files {
+		if f, err := os.Open(filepath.Join(dir, fi.Name())); err != nil {
+			t.Error(err)
+		} else {
+			runExample(t, f)
+		}
+	}
+}
+
+func runExample(t *testing.T, f *os.File) {
+	defer func() {
+		e := recover()
+		if e != nil && !strings.HasPrefix(filepath.Base(f.Name()), "err_") {
+			t.Error(e)
+		}
+		if e == nil && strings.HasPrefix(filepath.Base(f.Name()), "err_") {
+			t.Error(e)
+		}
+	}()
+
+	vm := New()
+	vm.Run(f)
+}
