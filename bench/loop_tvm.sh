802a17e1e5bb4cf2c2e53e5a05796b20d6227eff
Martin Angers <martin.n.angers@gmail.com>
2013-01-18 18:51:29 -0500

    make config and shell scripts usable on linux

--- bench/loop_tvm.sh
+++ bench/loop_tvm.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 for ((i=1; i <= $2 ; i++))
 do
-  ~/subwrkspc/tinyvm/bin/tvmi $GOPATH/src/github.com/PuerkitoBio/specter/cmd/examples/$1.vm
+  $SUBWRKSPC/tinyvm/bin/tvmi $GOPATH/src/github.com/PuerkitoBio/specter/cmd/examples/$1.vm
 done
--- bench/xtime.sh
+++ bench/xtime.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Copied from the blog post Profiling Go programs
 # http://blog.golang.org/2011/06/profiling-go-programs.html
@@ -6,8 +6,10 @@
 # On Mac OSX you have to install gnu-time (via Macports or Homebrew).
 #
 os=$(uname)
-if [[ "$os" != 'Darwin' ]]; then
-   alias gtime='time'
+if [[ "$os" == 'Darwin' ]]; then
+  echo user-time sys-time real-time max-mem cmd...
+  gtime -f '%Uu %Ss %er %MkB %C' "$@"
+else
+  echo user-time sys-time real-time max-mem cmd...
+  /usr/bin/time -f '%Uu %Ss %er %MkB %C' "$@"
 fi
-echo user-time sys-time real-time max-mem cmd...
-gtime -f '%Uu %Ss %er %MkB %C' "$@"
